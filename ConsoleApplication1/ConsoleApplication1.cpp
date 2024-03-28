eb5a137808731265054434d311667fdac4b836ac
Yulia Chernykh <ihavebeenworking@mail.ru>
2024-03-28 17:59:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	float at, Qp;
+
+	float Qt = at * Qp;
+	int nt = (Qt / qt)+1; 
+	cout << "Количество тестомешалок: " << nt;
+
+	float Qf = (1 - at) * Qp;
+	int  nf = (Qf / qf)+1;
+	cout << "Количество куттеров: " << nf;
+	
+	cout << "Количество машин для создания пельменей: " << np;
+
+}
